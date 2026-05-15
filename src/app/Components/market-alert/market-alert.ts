378d4493f68f2b834dadabfad98ce074a385863e
nancyalgazzar <nancyayman89@gmail.com>
2026-05-15 19:59:44 +0300

    ui fix2

---
+++ src/app/Components/market-alert/market-alert.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-alert',
+  imports: [],
+  templateUrl: './market-alert.html',
+  styleUrl: './market-alert.css',
+})
+export class MarketAlert {}
