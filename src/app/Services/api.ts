02f45f67227fc97db14698a4e211d5c82c858861
Omar Madbouly <omarsayed.cpp@gmail.com>
2026-05-18 03:17:08 +0300

    Create API Service File

---
+++ src/app/Services/api.ts
@@ -0,0 +1,16 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private http = inject(HttpClient);
+  private baseUrl = 'http://localhost:3000';
+
+  // Fetches a user's logged investment records from the relation link map
+  getUserInvestments(userId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/user_projects?userId=${userId}`);
+  }
+}
