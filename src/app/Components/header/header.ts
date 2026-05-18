2ec0fe393b57a5496e14432b7f41c681da6630c0
Omar Madbouly <omarsayed.cpp@gmail.com>
2026-05-18 03:21:41 +0300

    Make the Header Component dynamic with dummy data

--- src/app/Components/header/header.ts
+++ src/app/Components/header/header.ts
@@ -1,5 +1,6 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from './../../Services/api';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
-  totalNetWorth: number = 1000000;
-  avaliableLiquidity: number = 500000;
+export class Header implements OnInit {
+  private apiService = inject(ApiService);
 
-  
+  totalNetWorth: number = 0;
+  avaliableLiquidity: number = 500000; // Left static for this iteration chunk
+
+  ngOnInit() {
+    // Read local database for test mock user id 1
+    this.apiService.getUserInvestments(1).subscribe({
+      next: (investments) => {
+        // Automatically aggregate total balances on layout generation
+        this.totalNetWorth = investments.reduce((sum, current) => sum + current.invested_amount, 0);
+      }
+    });
+  }
 }
