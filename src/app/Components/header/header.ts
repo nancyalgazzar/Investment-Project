d2ea7ad007575b74dd0b9613c977a9456ffa6bce
Omar Madbouly <omarsayed.cpp@gmail.com>
2026-05-18 03:48:53 +0300

    feat: successfully connected dashboard header to dynamic JSON API

--- src/app/Components/header/header.ts
+++ src/app/Components/header/header.ts
@@ -1,7 +1,6 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { ApiService } from './../../Services/api';
-
 @Component({
   selector: 'app-header',
   imports: [CurrencyPipe],
@@ -10,16 +9,16 @@ import { ApiService } from './../../Services/api';
 })
 export class Header implements OnInit {
   private apiService = inject(ApiService);
+  private cdr = inject(ChangeDetectorRef);
 
   totalNetWorth: number = 0;
-  avaliableLiquidity: number = 500000; // Left static for this iteration chunk
+  avaliableLiquidity: number = 500000;
 
   ngOnInit() {
-    // Read local database for test mock user id 1
     this.apiService.getUserInvestments(1).subscribe({
       next: (investments) => {
-        // Automatically aggregate total balances on layout generation
-        this.totalNetWorth = investments.reduce((sum, current) => sum + current.invested_amount, 0);
+        this.totalNetWorth = investments.reduce((sum, item) => sum + Number(item.invested_amount), 0);
+        this.cdr.detectChanges();
       }
     });
   }
