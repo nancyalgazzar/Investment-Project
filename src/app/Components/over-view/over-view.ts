d65998b4171e5fa8fc727a13cd437aa1f10c9634
Omar Madbouly <omarsayed.cpp@gmail.com>
2026-05-18 03:54:29 +0300

    feat: successfully dashboard piechart and active assets to real JSON API

--- src/app/Components/over-view/over-view.ts
+++ src/app/Components/over-view/over-view.ts
@@ -1,46 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { BaseChartDirective } from 'ng2-charts';
-import { Plugin  } from 'chart.js';
+import { Plugin } from 'chart.js';
+import { ApiService } from './../../Services/api';
+
 @Component({
   standalone: true,
   selector: 'app-over-view',
   imports: [BaseChartDirective],
-
   templateUrl: './over-view.html',
   styleUrl: './over-view.css',
 })
-export class OverView {
-  chartData = {
-    datasets: [
-      {
-        data: [70, 30],
-        backgroundColor: ['rgba(202, 189, 41, 0.96)', 'rgb(19, 122, 191)'],
-        cutout: '80%',
-      },
-    ],
+export class OverView implements OnInit {
+  private apiService = inject(ApiService);
+  private cdr = inject(ChangeDetectorRef);
+
+  activeAssetsCount: number = 0;
+
+  // Default blank state for the chart
+  chartData: any = {
+    labels: [],
+    datasets: [{
+      data: [0],
+      backgroundColor: ['#232323'],
+      cutout: '80%',
+    }],
   };
+
   chartOptions = {
     responsive: true,
+    maintainAspectRatio: false
   };
-centerTextPlugin: Plugin<'doughnut'> = {
+
+  ngOnInit() {
+    this.apiService.getUserInvestments(1).subscribe({
+      next: (investments) => {
+        this.activeAssetsCount = investments.length; // Counts how many projects you have
+
+        if (investments.length > 0) {
+          // This grabs JUST the money amounts to draw the chart slices
+          const variableDataPoints = investments.map(item => Number(item.invested_amount));
+
+          this.chartData = {
+            datasets: [{
+              data: variableDataPoints,
+              backgroundColor: ['#D4AF37', '#137ABF', '#4edea3'],
+              cutout: '80%',
+            }],
+          };
+        }
+
+        this.cdr.detectChanges(); // Tell Angular to redraw the chart!
+      }
+    });
+  }
+
+  // This plugin draws the text in the middle of the doughnut hole
+  centerTextPlugin: Plugin<'doughnut'> = {
     id: 'centerText',
     afterDraw(chart) {
       const { ctx, chartArea: { top, width, height } } = chart;
-
       ctx.save();
-
-      // ✅ main text
       ctx.font = '1em Arial';
       ctx.fillStyle = 'grey';
       ctx.textAlign = 'center';
       ctx.textBaseline = 'middle';
       ctx.fillText('Profile', width / 2, top + height / 2 - 10);
 
-      // ✅ sub text below
-      ctx.font = ' bold 2em Arial';
+      ctx.font = 'bold 2em Arial';
       ctx.fillStyle = 'white';
-      ctx.fillText('30/70', width / 2, top + height / 2 + 20);
-
+      ctx.fillText('Active', width / 2, top + height / 2 + 20);
       ctx.restore();
     }
   };
