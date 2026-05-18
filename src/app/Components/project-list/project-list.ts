1b165cfa268d3a2254b556c87b91cf54bd4e3f2c
FatmaHaggag <fatmamohamed3890@gmail.com>
2026-05-18 03:41:33 +0300

    added project list

--- src/app/Components/project-list/project-list.ts
+++ src/app/Components/project-list/project-list.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from "../header/header";
 import { SideBar } from "../side-bar/side-bar";
 import { RouterOutlet } from "@angular/router";
+import { ProjectCard } from "../project-card/project-card";
 
 @Component({
   selector: 'app-project-list',
-  imports: [Header, SideBar, RouterOutlet],
+  imports: [Header, SideBar, RouterOutlet, ProjectCard],
   templateUrl: './project-list.html',
   styleUrl: './project-list.css',
 })
